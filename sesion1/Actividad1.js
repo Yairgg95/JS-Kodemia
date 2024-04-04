9c3688d0d1163e78db75c564b79319d3be398f2b
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-04-03 20:55:44 -0600

    tarea primera sesion

--- sesion1/Actividad1.js
+++ sesion1/Actividad1.js
@@ -1,9 +1,22 @@
 // monto previsto para ese mismo dìa
-const montoPrevisto = 5;
+const montoPrevisto = 10;
 // Crear una variable para guardar no. de ordenes de un dia
-const ordenesEfectivas = 11;
+const ordenesEfectivas = 17;
+// variable para diaDelMes
+const diaDelMes = 3;
 
-function proyectarCapacidad() {
+function esPar(diaDelMes){
+   return diaDelMes % 2 === 0;
+} 
+
+function calcularProporcionCumplidas(ordenesEfectivas, montoPrevisto) {
+    return (montoPrevisto / ordenesEfectivas) * 100;
+}
+
+
+function proyectarCapacidad(diaDelMes) {
+
+    const esDiaPar = esPar(diaDelMes);
 
 if (ordenesEfectivas > montoPrevisto ) {
     console.log("Debes incrementar la capacidad en el dìa siguiente");
@@ -13,6 +26,15 @@ console.log("Debes reducir la capacidad");
 } else {
     console.log("Estas bien sigue así!");
 }
+
+if (!esDiaPar && ordenesEfectivas > montoPrevisto) {
+    console.log("El día es impar, es probable una mayor demanda el dìa siguiente")
+} 
+
+const proporcionCumplidas = calcularProporcionCumplidas(montoPrevisto, ordenesEfectivas);
+    console.log("Proporción de órdenes cumplidas: " + proporcionCumplidas.toFixed(2) + "%");
+
 }
 
-proyectarCapacidad ();
+proyectarCapacidad (ordenesEfectivas, montoPrevisto, diaDelMes);
+
